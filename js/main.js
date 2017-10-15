996e7c8c02235cbc6f3e90bc17e90f9a4c98798a
Aaditya Deowanshi <aadityadeowanshi@hotmail.com>
2017-10-14 19:42:28 -0700

    Added eduction and work

--- js/main.js
+++ js/main.js
@@ -1,43 +1,66 @@
-(function($) {
+(function ($) {
   "use strict";
-  $(window).on("load", function() { // makes sure the whole site is loaded
+  $(window).on("load", function () { // makes sure the whole site is loaded
     //preloader
     $("#status").fadeOut(); // will first fade out the loading animation
     $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
-    
+
     //masonry
     $('.grid').masonry({
       itemSelector: '.grid-item'
-      
-    });    
+
+    });
+  });
+  $(document).ready(function () {
+    var message = "";
+    $('#formBlock').on('click', function (e) {
+        //pretend we don't need validation
+      message = $("#message").serialize();
+      //send to formspree
+      $.ajax({
+        url: 'http://formspree.io/aadityadeowanshi@hotmail.com',
+        method: "POST",
+        data: {
+          message: message
+        },
+        dataType: "json",
+        success: function () {
+          console.log('success');
+          $('#formBlock').hide();
+          $('#thankyouBlock').show();
+        }
+
+      });
+
+    });
+
   });
 
-
-  $(document).ready(function(){  
+  $(document).ready(function () {
 
     //active menu
     $(document).on("scroll", onScroll);
- 
+
     $('a[href^="#"]').on('click', function (e) {
       e.preventDefault();
       $(document).off("scroll");
- 
+
       $('a').each(function () {
         $(this).removeClass('active');
       })
       $(this).addClass('active');
- 
+
       var target = this.hash;
       $target = $(target);
       $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
+        'scrollTop': $target.offset().top + 2
       }, 500, 'swing', function () {
         window.location.hash = target;
         $(document).on("scroll", onScroll);
       });
     });
 
-    
+
     //scroll js
     smoothScroll.init({
       selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
@@ -46,41 +69,40 @@
       easing: 'easeInOutCubic', // Easing pattern to use
       updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
       offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
-      callback: function ( toggle, anchor ) {} // Function to run after scrolling
+      callback: function (toggle, anchor) {} // Function to run after scrolling
     });
 
     //menu
     var bodyEl = document.body,
-    content = document.querySelector( '.content-wrap' ),
-    openbtn = document.getElementById( 'open-button' ),
-    closebtn = document.getElementById( 'close-button' ),
-    isOpen = false;
+      content = document.querySelector('.content-wrap'),
+      openbtn = document.getElementById('open-button'),
+      closebtn = document.getElementById('close-button'),
+      isOpen = false;
 
     function inits() {
       initEvents();
     }
 
     function initEvents() {
-      openbtn.addEventListener( 'click', toggleMenu );
-      if( closebtn ) {
-        closebtn.addEventListener( 'click', toggleMenu );
+      openbtn.addEventListener('click', toggleMenu);
+      if (closebtn) {
+        closebtn.addEventListener('click', toggleMenu);
       }
 
       // close the menu element if the target it´s not the menu element or one of its descendants..
-      content.addEventListener( 'click', function(ev) {
+      content.addEventListener('click', function (ev) {
         var target = ev.target;
-        if( isOpen && target !== openbtn ) {
+        if (isOpen && target !== openbtn) {
           toggleMenu();
         }
-      } );
+      });
     }
 
     function toggleMenu() {
-      if( isOpen ) {
-        classie.remove( bodyEl, 'show-menu' );
-      }
-      else {
-        classie.add( bodyEl, 'show-menu' );
+      if (isOpen) {
+        classie.remove(bodyEl, 'show-menu');
+      } else {
+        classie.add(bodyEl, 'show-menu');
       }
       isOpen = !isOpen;
     }
@@ -90,45 +112,44 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My name is Aaditya", "I'm a Software Developer", "Looking for Internship"],
-        typeSpeed: 100,
-        backDelay: 900,
-        // loop
-        loop: true
+      strings: ["My name is Aaditya", "I'm a Software Developer", "Looking for Internship"],
+      typeSpeed: 100,
+      backDelay: 900,
+      // loop
+      loop: true
     });
 
     //owl carousel
     $('.owl-carousel').owlCarousel({
       autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 1,
-      itemsDesktop : [1199,1],
-      itemsDesktopSmall : [979,1],
-      itemsTablet : [768,1],
-      itemsMobile : [479,1],
+
+      items: 1,
+      itemsDesktop: [1199, 1],
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768, 1],
+      itemsMobile: [479, 1],
 
       // CSS Styles
-      baseClass : "owl-carousel",
-      theme : "owl-theme"
+      baseClass: "owl-carousel",
+      theme: "owl-theme"
     });
 
     $('.owl-carousel2').owlCarousel({
       autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 1,
-      itemsDesktop : [1199,1],
-      itemsDesktopSmall : [979,1],
-      itemsTablet : [768,1],
-      itemsMobile : [479,1],
-      autoPlay : false,
+
+      items: 1,
+      itemsDesktop: [1199, 1],
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768, 1],
+      itemsMobile: [479, 1],
 
       // CSS Styles
-      baseClass : "owl-carousel",
-      theme : "owl-theme"
+      baseClass: "owl-carousel",
+      theme: "owl-theme"
     });
 
     //contact
-    $('input').blur(function() {
+    $('input').blur(function () {
 
       // check if the input has any value (if we've typed into it)
       if ($(this).val())
@@ -145,42 +166,51 @@
     //   }
     //   // other options
     // });
-    
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+      $('#modal-01').modal('hide');
+      $('#modal-02').modal('hide');
+      $('#modal-03').modal('hide');
+      $('#modal-04').modal('hide');
+      $('#modal-05').modal('hide');
+      $('#modal-06').modal('hide');
+
+    });
+
     //Skill
-    jQuery('.skillbar').each(function() {
-      jQuery(this).appear(function() {
+    jQuery('.skillbar').each(function () {
+      jQuery(this).appear(function () {
         jQuery(this).find('.count-bar').animate({
-          width:jQuery(this).attr('data-percent')
-        },3000);
+          width: jQuery(this).attr('data-percent')
+        }, 3000);
         var percent = jQuery(this).attr('data-percent');
         jQuery(this).find('.count').html('<span>' + percent + '</span>');
       });
-    }); 
+    });
 
-  
+
   });
-  
-    
+
+
   //header
   function inits() {
-    window.addEventListener('scroll', function(e){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
-            header = document.querySelector(".for-sticky");
-        if (distanceY > shrinkOn) {
-            classie.add(header,"opacity-nav");
-        } else {
-            if (classie.has(header,"opacity-nav")) {
-                classie.remove(header,"opacity-nav");
-            }
-          }
-      });
-    }
+    window.addEventListener('scroll', function (e) {
+      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 300,
+        header = document.querySelector(".for-sticky");
+      if (distanceY > shrinkOn) {
+        classie.add(header, "opacity-nav");
+      } else {
+        if (classie.has(header, "opacity-nav")) {
+          classie.remove(header, "opacity-nav");
+        }
+      }
+    });
+  }
 
   window.onload = inits();
 
   //nav-active
-  function onScroll(event){
+  function onScroll(event) {
     var scrollPosition = $(document).scrollTop();
     $('.menu-list a').each(function () {
       var currentLink = $(this);
@@ -188,8 +218,7 @@
       if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
         $('.menu-list a').removeClass("active");
         currentLink.addClass("active");
-      }
-      else{
+      } else {
         currentLink.removeClass("active");
       }
     });
